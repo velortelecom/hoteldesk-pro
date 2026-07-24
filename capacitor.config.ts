83684a7591db7026d134224ad7dd48c370555ce5
velortelecom <velor.telecom@gmail.com>
2026-07-24 12:32:00 +0200

    Add initial Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.velor.hoteldeskpro',
+    appName: 'Velor One',
+    webDir: 'build',
+    server: {
+    androidScheme: 'https'
+      }
+};
+
+export default config;
